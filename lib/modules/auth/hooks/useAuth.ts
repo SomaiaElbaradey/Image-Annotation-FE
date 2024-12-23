f5d2e8d5ddb089ee07b79e0ba7284420b9d9e5fa
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-23 06:33:13 +0200

    style: use auth hook

---
+++ lib/modules/auth/hooks/useAuth.ts
@@ -0,0 +1,106 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+
+import app from "@/lib/server/firebase/config";
+import { LoginInputs, RegisterInputs } from "@/lib/modules/auth/schemas/auth";
+import { auth } from "@/lib/server/firebase";
+import { deleteSessionCookie, setSessionCookie } from "@/lib/server/cookie";
+
+type UnAuthedState = {
+    state: "error";
+    message: string;
+};
+
+type AuthedState = {
+    state: "authenticated";
+};
+
+type LoadingState = {
+    state: "loading";
+};
+
+type AuthState = UnAuthedState | AuthedState | LoadingState;
+
+export const useAuth = () => {
+    const [authState, setAuthState] = useState<AuthState>();
+    const router = useRouter();
+
+    const login = async (data: LoginInputs) => {
+        setAuthState({ state: "loading" });
+
+        try {
+            const auth = getAuth(app);
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+
+            auth.onAuthStateChanged(async (user) => {
+                if (user) {
+                    await setSessionCookie("user", JSON.stringify(user));
+                    router.push("/tasks");
+                } else {
+                    deleteSessionCookie("user");
+                    router.push("/login");
+                }
+            });
+
+            setAuthState({ state: "authenticated" });
+        } catch (err) {
+            setAuthState({
+                state: "error",
+                message:
+                    (err as Error).message ||
+                    "An error occurred during sign in",
+            });
+        }
+    };
+
+    const register = async (data: RegisterInputs) => {
+        setAuthState({ state: "loading" });
+
+        try {
+            const auth = getAuth(app);
+            await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            );
+            router.push("/login");
+        } catch (err) {
+            setAuthState({
+                state: "error",
+                message:
+                    (err as Error).message ||
+                    "An error occurred during registration",
+            });
+        }
+    };
+
+    const logout = async () => {
+        setAuthState({ state: "loading" });
+
+        try {
+            await signOut(auth);
+            deleteSessionCookie("user");
+            router.push("/login");
+        } catch (err) {
+            setAuthState({
+                state: "error",
+                message:
+                    (err as Error).message || "An error occurred during logout",
+            });
+        }
+    };
+
+    const authError = authState?.state === "error" ? authState.message : "";
+    const isLoading = authState?.state === "loading";
+
+    return { login, register, error: authError, isLoading, logout };
+};
