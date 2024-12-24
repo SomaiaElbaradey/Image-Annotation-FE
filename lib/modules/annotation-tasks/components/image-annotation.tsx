e6248331740db63f079ed878aeaf70d354b07904
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-24 20:16:55 +0200

    feat(component): canvas annotation

--- lib/modules/annotation-tasks/components/image-annotation.tsx
+++ lib/modules/annotation-tasks/components/image-annotation.tsx
@@ -1,16 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useTaskAnnotations } from "../hooks/useTaskAnnotations";
 
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { updateTaskImageUrl, uploadImage } from "@/lib/server/firebase";
+import { useImageUpload } from "../hooks/useImageUpload";
+import { useAuth } from "../../auth/hooks";
 
-import { useTaskAnnotations } from "../hooks";
 import AnnotationCanvas from "./annotation-canvas";
+import ImageUploader from "./image-uploader";
+import AnnotationList from "./annotation-list";
+
+
+const PLACEHOLDER_IMG = '/images/placeholder.png';
 
 export default function CanvasAnnotation() {
-    const [user, setUser] = useState<any>(null);
+    const { user } = useAuth();
     const {
         currentTaskIndex,
         tasks,
@@ -21,57 +25,30 @@ export default function CanvasAnnotation() {
         handleNextTask
     } = useTaskAnnotations(user?.uid);
 
-    useEffect(() => {
-        const auth = getAuth();
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser(user);
-            }
-        });
+    const currentTask = tasks?.[currentTaskIndex || 0];
+    const { uploadedUrl, handleFileChange, handleUpload, uploading } = useImageUpload(
+        currentTask?.imageURL || PLACEHOLDER_IMG,
+        currentTask?.id || ''
+    );
 
-        return () => unsubscribe();
-    }, []);
-
-    const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            try {
-                const imageUrl = await uploadImage(file);
-                await updateTaskImageUrl(tasks[currentTaskIndex].id, imageUrl);
-
-                // setTasks(prevTasks =>
-                //     prevTasks.map((task, index) =>
-                //         index === currentTaskIndex
-                //             ? { ...task, imageUrl }
-                //             : task
-                //     )
-                // );
-
-                // redrawCanvas();
-            } catch (err) {
-                console.error(err);
-                // setError('Failed to upload image. Please try again.');
-            }
-        }
-    };
-
-    if (!user) {
+    if (!user)
         return <div>Loading...</div>;
-    }
 
-    if (tasks.length === 0) {
-        return <div>No tasks available.</div>;
-    }
+    if (tasks.length === 0)
+        return <div>No tasks available.</div>
 
     return (
         <div className="flex flex-col items-center mt-3">
-            <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageUpload}
-                className="mb-4"
+            <ImageUploader
+                handleFileChange={handleFileChange}
+                handleUpload={handleUpload}
+                uploading={uploading}
             />
-            <AnnotationCanvas annotations={annotations} setAnnotations={setAnnotations} />
+            <AnnotationCanvas
+                annotations={annotations}
+                setAnnotations={setAnnotations}
+                imageUrl={uploadedUrl}
+            />
             <button
                 onClick={handleSave}
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 mt-3"
@@ -79,20 +56,11 @@ export default function CanvasAnnotation() {
                 Save Annotations
             </button>
             {error && <p className="text-red-500 mt-2">{error}</p>}
-            <div className="mt-4">
-                <h3 className="text-xl font-bold mb-2">Current Annotations:</h3>
-                <ul>
-                    {annotations.map((annotation) => (
-                        <li key={annotation.id}>
-                            Rectangle at ({annotation.x.toFixed(2)}, {annotation.y.toFixed(2)}) with size
-                            {annotation.width.toFixed(2)}x{annotation.height.toFixed(2)}: {annotation.text}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <AnnotationList annotations={annotations} />
             <button
                 onClick={handleNextTask}
                 className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-3"
+                disabled={currentTaskIndex === tasks.length - 1}
             >
                 Next Task
             </button>
