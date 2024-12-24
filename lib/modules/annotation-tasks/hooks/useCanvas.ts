a21ea33966bacb647739951183143b1b64ef204d
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-24 20:15:06 +0200

    style(hook): separate use canvas hook

--- lib/modules/annotation-tasks/hooks/useCanvas.ts
+++ lib/modules/annotation-tasks/hooks/useCanvas.ts
@@ -5,48 +5,50 @@ import { Annotation } from "../schemas";
 
 export const useCanvas = (
     annotations: Annotation[],
-    setAnnotations: React.Dispatch<React.SetStateAction<Annotation[]>>
+    setAnnotations: (annotations: Annotation[]) => void,
+    imageUrl: string
 ) => {
     const [currentRect, setCurrentRect] = useState<Annotation | null>(null);
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    const redrawCanvas = useCallback(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
+    const redrawCanvas = useCallback(
+        (imageUrl: string) => {
+            const canvas = canvasRef.current;
+            if (!canvas) return;
 
-        const ctx = canvas.getContext("2d");
-        if (!ctx) return;
+            const ctx = canvas.getContext("2d");
+            if (!ctx) return;
 
-        const img = new Image();
-        img.src =
-            "https://imgs.search.brave.com/4HiZ_iW1k5mmsmDIH_C05H0HZeLjEx3bSapAaNcDixo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA5LzYwLzkyLzIz/LzM2MF9GXzk2MDky/MjMwNl9ySFBiZTJB/Q21jYW83WjJLaUdY/Tm9EVk11VmxQZGRT/Ty5qcGc";
-        img.onload = () => {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
+            const img = new Image();
+            img.src = imageUrl;
 
-            annotations.forEach(({ x, y, width, height, text }) => {
-                ctx.strokeStyle = "red";
-                ctx.lineWidth = 2;
-                ctx.strokeRect(x, y, width, height);
+            img.onload = () => {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
 
-                ctx.fillStyle = "red";
-                ctx.font = "12px Arial";
-                ctx.fillText(text, x, y - 5);
-            });
+                annotations.forEach(({ x, y, width, height, text }) => {
+                    ctx.strokeStyle = "red";
+                    ctx.lineWidth = 2;
+                    ctx.strokeRect(x, y, width, height);
 
-            if (currentRect) {
-                const { x, y, width, height } = currentRect;
-                ctx.strokeStyle = "blue";
-                ctx.lineWidth = 1;
-                ctx.strokeRect(x, y, width, height);
-            }
-        };
-    }, [annotations, currentRect]);
+                    ctx.fillStyle = "red";
+                    ctx.font = "12px Arial";
+                    ctx.fillText(text, x, y - 5);
+                });
 
-    useEffect(() => {
-        redrawCanvas();
-    }, [redrawCanvas]);
+                if (currentRect) {
+                    const { x, y, width, height } = currentRect;
+                    ctx.strokeStyle = "blue";
+                    ctx.lineWidth = 1;
+                    ctx.strokeRect(x, y, width, height);
+                }
+            };
+        },
+        [annotations, currentRect]
+    );
+
+    useEffect(() => redrawCanvas(imageUrl), [redrawCanvas, imageUrl]);
 
     const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
@@ -84,9 +86,9 @@ export const useCanvas = (
         if (currentRect) {
             const text = prompt("Enter annotation text:") || "";
             const annotatedRect = { ...currentRect, text };
-            setAnnotations((prev) => [...prev, annotatedRect]);
+            setAnnotations([...annotations, annotatedRect]);
             setCurrentRect(null);
-            redrawCanvas();
+            redrawCanvas(imageUrl);
         }
     };
 
@@ -96,5 +98,5 @@ export const useCanvas = (
         handleMouseMove,
         handleMouseUp,
         redrawCanvas,
-    };
+    } as const;
 };
