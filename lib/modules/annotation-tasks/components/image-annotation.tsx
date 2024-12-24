7e8258d590988050a7b99e1301f392c7ed029335
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-24 22:24:35 +0200

    feat: status filter

--- lib/modules/annotation-tasks/components/image-annotation.tsx
+++ lib/modules/annotation-tasks/components/image-annotation.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import React from "react";
-import { useTaskAnnotations } from "../hooks/useTaskAnnotations";
+import React, { useEffect } from "react";
 
+import { useTaskAnnotations } from "../hooks/useTaskAnnotations";
 import { useImageUpload } from "../hooks/useImageUpload";
 import { useAuth } from "../../auth/hooks";
 
@@ -10,6 +10,8 @@ import AnnotationCanvas from "./annotation-canvas";
 import ImageUploader from "./image-uploader";
 import AnnotationList from "./annotation-list";
 
+import { StatusFilter } from "../schemas";
+import TaskStatusFilter from "./Task-filter";
 
 const PLACEHOLDER_IMG = '/images/placeholder.png';
 
@@ -17,57 +19,83 @@ export default function CanvasAnnotation() {
     const { user } = useAuth();
     const {
         currentTaskIndex,
-        tasks,
+        filteredTasks,
         annotations,
         setAnnotations,
         error,
         handleSave,
-        handleNextTask
+        handleNextTask,
+        handlePreviousTask,
+        setCurrentTaskIndex,
+        setFilter,
+        currentFilter
     } = useTaskAnnotations(user?.uid);
 
-    const currentTask = tasks?.[currentTaskIndex || 0];
+    const currentTask = filteredTasks[currentTaskIndex];
     const { uploadedUrl, handleFileChange, handleUpload, uploading } = useImageUpload(
         currentTask?.imageURL || PLACEHOLDER_IMG,
         currentTask?.id || ''
     );
 
+    useEffect(() => {
+        if (filteredTasks.length === 0) {
+            setAnnotations([]);
+            setCurrentTaskIndex(0);
+        }
+    }, [filteredTasks, setAnnotations, setCurrentTaskIndex]);
+
     if (!user)
         return <div>Loading...</div>;
 
-    if (tasks.length === 0)
-        return <div>No tasks available.</div>
+    const handleStatusChange = (status: StatusFilter) => {
+        setFilter(status);
+    };
 
     return (
         <div className="flex flex-col items-center mt-3">
-            <ImageUploader
-                handleFileChange={handleFileChange}
-                handleUpload={handleUpload}
-                uploading={uploading}
-            />
-            <AnnotationCanvas
-                annotations={annotations}
-                setAnnotations={setAnnotations}
-                imageUrl={uploadedUrl}
-            />
-            <button
-                onClick={handleSave}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 mt-3"
-            >
-                Save Annotations
-            </button>
-            {error && <p className="text-red-500 mt-2">{error}</p>}
-            <AnnotationList annotations={annotations} />
-            <button
-                onClick={handleNextTask}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-3"
-                disabled={currentTaskIndex === tasks.length - 1}
-            >
-                Next Task
-            </button>
-            <div className="mt-4">
-                <h3 className="text-xl font-bold mb-2">Task Progress:</h3>
-                <p>{currentTaskIndex + 1} of {tasks.length}</p>
-            </div>
+            <TaskStatusFilter currentStatus={currentFilter} onStatusChange={handleStatusChange} />
+            {filteredTasks.length > 0 ?
+                <>
+                    <ImageUploader
+                        handleFileChange={handleFileChange}
+                        handleUpload={handleUpload}
+                        uploading={uploading}
+                    />
+                    <AnnotationCanvas
+                        annotations={annotations}
+                        setAnnotations={setAnnotations}
+                        imageUrl={uploadedUrl}
+                    />
+                    <button
+                        onClick={() => handleSave()}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 mt-3"
+                    >
+                        Save Annotations
+                    </button>
+                    {error && <p className="text-red-500 mt-2">{error}</p>}
+                    <AnnotationList annotations={annotations} />
+                    <div className="flex justify-between w-full max-w-lg mt-4">
+                        <button
+                            onClick={handlePreviousTask}
+                            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                            disabled={currentTaskIndex === 0}
+                        >
+                            Previous Task
+                        </button>
+                        <button
+                            onClick={handleNextTask}
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            disabled={currentTaskIndex === filteredTasks.length - 1}
+                        >
+                            Next Task
+                        </button>
+                    </div>
+                    <div className="mt-4">
+                        <h3 className="text-xl font-bold mb-2">Task Progress:</h3>
+                        <p>{currentTaskIndex + 1} of {filteredTasks.length}</p>
+                    </div>
+                </>
+                : <div>No tasks available for the current filter.</div>}
         </div>
     );
 }
