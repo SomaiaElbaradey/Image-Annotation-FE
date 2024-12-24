bbf37e211e0e115f48530f7416166ebe70ead706
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-24 20:18:03 +0200

    fix: upload image util

--- lib/server/firebase/utils.ts
+++ lib/server/firebase/utils.ts
@@ -1,23 +1,34 @@
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { doc, updateDoc } from "firebase/firestore";
 
 import { v4 as uuidv4 } from "uuid";
 
-import db from "@/lib/server/firebase";
+import db, { storage } from "@/lib/server/firebase";
 import { Annotation } from "@/lib/modules/annotation-tasks/schemas";
 
-export const uploadImage = async (file: File): Promise<string> => {
-    const storage = getStorage();
+export const uploadImage = async (
+    file: File | undefined
+): Promise<string | undefined> => {
+    if (!file) return;
+
     const fileId = uuidv4();
     const fileRef = ref(storage, `task-images/${fileId}`);
-    await uploadBytes(fileRef, file);
-    const downloadURL = await getDownloadURL(fileRef);
-    return downloadURL;
+
+    try {
+        await uploadBytes(fileRef, file);
+        const url = await getDownloadURL(fileRef);
+        return url;
+    } catch (error) {
+        console.error("Error uploading the file", error);
+    }
 };
 
-export const updateTaskImageUrl = async (taskId: string, imageUrl: string) => {
+export const updateTaskImageUrl = async (taskId: string, imageURL: string) => {
     const taskRef = doc(db, "tasks", taskId);
-    await updateDoc(taskRef, { imageUrl });
+    console.log("taskRef", taskRef);
+    console.log("imageURL", imageURL);
+
+    await updateDoc(taskRef, { imageURL });
 };
 
 export const saveAnnotations = async (
