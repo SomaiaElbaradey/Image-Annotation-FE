125472cc37c77d669e0c73897cecc5d36418606f
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-25 16:05:05 +0200

    chore: config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,31 @@ export default {
     theme: {
         extend: {
             colors: {
-                background: "var(--background)",
                 foreground: "var(--foreground)",
+                primary: {
+                    DEFAULT: "#0066c0",
+                    900: "#001F3F",
+                    700: "#00509E",
+                },
+                white: "#FFFFFF",
+                danger: "#e90d00",
+                backgroundMain: {
+                    disabled: "#E8F0FE",
+                    mask: "#0A2742",
+                },
+                backgroundAccent: {
+                    warningLight: "#FFF8E1",
+                    dangerLight: "#FFEBEE",
+                    positiveLight: "#E8F5E9",
+                },
+                content: {
+                    primary: "#002855",
+                    secondary: "#4B6A92",
+                    disabled: "#9FB3C8",
+                },
+                borderColor: {
+                    DEFAULT: "#D1D9E6",
+                },
             },
         },
     },
