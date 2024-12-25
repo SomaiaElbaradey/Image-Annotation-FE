404f422b7f6d528d11154c82a75904a8b5c66693
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-25 22:03:50 +0200

    refactor: task filter using shadcn ui

--- lib/modules/annotation-tasks/components/task-filter.tsx
+++ lib/modules/annotation-tasks/components/task-filter.tsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import { StatusFilter } from '../schemas';
+import React from 'react'
+
+import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@/lib/ui'
+
+import { StatusFilter } from '../schemas'
+import { ChevronRight } from '@/lib/ui/icons'
 
 interface TaskStatusFilterProps {
-    currentStatus: StatusFilter;
-    onStatusChange: (status: StatusFilter) => void;
+    currentStatus: StatusFilter
+    onStatusChange: (status: StatusFilter) => void
 }
 
-function TaskStatusFilter({ currentStatus, onStatusChange }: TaskStatusFilterProps) {
-    const statuses: StatusFilter[] = ['All', 'Pending', 'In Progress', 'Completed'];
+const statuses: StatusFilter[] = ['All', 'Pending', 'In Progress', 'Completed']
 
+export default function TaskStatusFilter({ currentStatus, onStatusChange }: TaskStatusFilterProps) {
     return (
-        <div className="mb-4">
-            <select
-                value={currentStatus}
-                onChange={(event) => onStatusChange(event?.target.value as StatusFilter)}
-                className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none 
-                        focus:ring-blue-500 focus:border-blue-500"
-            >
-                {statuses.map((status) => (
-                    <option key={status} value={status}>
-                        {status}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
-};
-
-export default TaskStatusFilter;
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='tertiary' size='small' className="w-[25%] items-center" trailingIcon={<ChevronRight></ChevronRight>}>
+                    {currentStatus} Tasks
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[200px]">
+                <DropdownMenuRadioGroup value={currentStatus} onValueChange={(status) => onStatusChange(status as StatusFilter)}>
+                    {statuses.map((status) => (
+                        <DropdownMenuRadioItem
+                            key={status}
+                            value={status}
+                            className="px-2 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
+                        >
+                            {status}
+                        </DropdownMenuRadioItem>
+                    ))}
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
