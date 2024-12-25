8d63786dc07ba23771b0f20c255913dcbfaeb16c
somaiaelbaradey <somayaelbaradey@gmail.com>
2024-12-25 22:01:14 +0200

    chore: layout fixes

--- lib/ui/icons/chevron-right.tsx
+++ lib/ui/icons/chevron-right.tsx
@@ -9,8 +9,8 @@ const ChevronRight = ({
 }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
+    width={20}
+    height={20}
     fill="none"
     viewBox="0 0 24 24"
     aria-labelledby={titleId}
@@ -21,7 +21,7 @@ const ChevronRight = ({
       fillRule="evenodd"
       clipRule="evenodd"
       d="M14.8467 12.0001L8.38228 5.30811C7.87225 4.78013 7.87225 3.9241 8.38228 3.39611C8.89231 2.86813 9.71924 2.86813 10.2293 3.39611L17.6172 11.0441C18.1273 11.5721 18.1273 12.4281 17.6172 12.9561L10.2293 20.6041C9.71924 21.1321 8.89231 21.1321 8.38228 20.6041C7.87225 20.0761 7.87225 19.2201 8.38228 18.6921L14.8467 12.0001Z"
-      fill={props.color || '#5E6160'}
+      fill={props.color || '#0e2e69'}
     />
   </svg>
 )
